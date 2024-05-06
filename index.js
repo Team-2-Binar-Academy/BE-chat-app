5720b0f2f3ee60c70779c1e24af87edf07a936bd
Dwi875 <dwix2504@gmail.com>
2024-05-06 22:02:19 +0700

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+require("dotenv").config();
 
-export const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
