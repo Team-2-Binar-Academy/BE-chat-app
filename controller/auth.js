57f37a38c5d7ec63ef1de9f7b056beaf76c45bc2
Muhammad Dwi Syamsurrijal <147080152+dwisyamsurrijal@users.noreply.github.com>
2024-05-15 23:41:15 +0700

    commit2 (#2)

--- controller/auth.js
+++ controller/auth.js
@@ -1,70 +1,94 @@
 const { register, login } = require("../usecase/auth");
 
 exports.register = async (req, res, next) => {
-    try {
-        // get the body
-        const { email, password, name } = req?.body;
+  try {
+    // get the body
+    const { email, password, name } = req?.body;
 
-        if (email == "" || !email) {
-            return next({
-                message: "Email must be filled!",
-                statusCode: 400,
-            });
-        }
-        if (password == "" || !password) {
-            return next({
-                message: "Password must be filled!",
-                statusCode: 400,
-            });
-        }
-        if (name == "" || !name) {
-            return next({
-                message: "Name must be filled!",
-                statusCode: 400,
-            });
-        }
-
-        const data = await register({
-            email,
-            password,
-            name,
-        });
-
-        res.status(200).json({
-            message: "Success",
-            data,
-        });
-    } catch (error) {
-        next(error);
+    if (email == "" || !email) {
+      return next({
+        message: "Email must be filled!",
+        statusCode: 400,
+      });
     }
+    if (password == "" || !password) {
+      return next({
+        message: "Password must be filled!",
+        statusCode: 400,
+      });
+    }
+    if (name == "" || !name) {
+      return next({
+        message: "Name must be filled!",
+        statusCode: 400,
+      });
+    }
+
+    const data = await register({
+      email,
+      password,
+      name,
+    });
+
+    res.status(200).json({
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.login = async (req, res, next) => {
-    try {
-        // get the body
-        const { email, password } = req.body;
+  try {
+    // get the body
+    const { email, password } = req.body;
 
-        if (email == "" || !email) {
-            return next({
-                message: "Email must be filled!",
-                statusCode: 400,
-            });
-        }
-        if (password == "" || !password) {
-            return next({
-                message: "Password must be filled!",
-                statusCode: 400,
-            });
-        }
-
-        // login logic
-        const data = await login(email, password);
-
-        res.status(200).json({
-            message: "Success",
-            data,
-        });
-    } catch (error) {
-        next(error);
+    if (email == "" || !email) {
+      return next({
+        message: "Email must be filled!",
+        statusCode: 400,
+      });
     }
+    if (password == "" || !password) {
+      return next({
+        message: "Password must be filled!",
+        statusCode: 400,
+      });
+    }
+
+    // login logic
+    const data = await login(email, password);
+
+    res.status(200).json({
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.googleLogin = async (req, res, next) => {
+  try {
+    // get the body
+    const { access_token } = req.body;
+
+    if (!access_token) {
+      return next({
+        statusCode: 400,
+        message: "Access token must be provided!",
+      });
+    }
+
+    // login with google logic
+    const data = await googleLogin(access_token);
+
+    res.status(200).json({
+      message: "Success",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
