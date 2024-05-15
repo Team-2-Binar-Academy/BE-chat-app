951f7a1d0bcf2e534b53f6dfa0107c9a07bdefdf
Dwi875 <dwix2504@gmail.com>
2024-05-15 23:40:09 +0700

    commit2

--- usecase/auth/index.js
+++ usecase/auth/index.js
@@ -63,3 +63,30 @@ exports.login = async (email, password) => {
 
     return data;
 };
+
+exports.googleLogin = async (accessToken) => {
+  // validate the token and get the data from google
+  const googleData = await getGoogleAccessTokenData(accessToken);
+
+  // get is there any existing user with the email
+  let user = await getUserByEmail(googleData?.email);
+
+  // if not found
+  if (!user) {
+    // Create new user based on google data that get by access_token
+    user = await createUser({
+      email: googleData?.email,
+      password: "",
+      name: googleData?.name,
+      picture: googleData?.picture,
+    });
+  }
+
+  // Delete object password from user
+  delete user?.dataValues?.password;
+
+  // create token
+  const data = createToken(user);
+
+  return data;
+};
